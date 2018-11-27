e315db4ec5e1786453eff8fdbeedb5dbd774ae00
fanzezhen <fanzezhen@niwodai.net>
2018-11-27 18:09:53 +0800

    https支持

--- startup/src/main/java/cn/fzz/springbootdemo/SpringbootDemoApplication.java
+++ startup/src/main/java/cn/fzz/springbootdemo/SpringbootDemoApplication.java
@@ -5,12 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @ComponentScan(basePackages = {"cn.fzz"})
 @SpringBootApplication
 @EntityScan("cn.fzz.bean")
 @EnableJpaRepositories("cn.fzz.dao")
 public class SpringbootDemoApplication {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
