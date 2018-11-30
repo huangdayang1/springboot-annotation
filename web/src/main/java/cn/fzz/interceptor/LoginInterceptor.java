678431624d44d5e24b4613bc3565b39e313cef25
andy <fanzezhen@niwodai.net>
2018-11-30 17:01:01 +0800

    静态资源拦截问题

--- web/src/main/java/cn/fzz/interceptor/LoginInterceptor.java
+++ web/src/main/java/cn/fzz/interceptor/LoginInterceptor.java
@@ -21,12 +21,6 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     private Logger log = Logger.getLogger(String.valueOf(LoginInterceptor.class));
 
-    /*@Autowired
-    UserService userService;*/
-
-    /*@Value("${IGNORE_LOGIN}")
-    Boolean IGNORE_LOGIN;*/
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
@@ -39,33 +33,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
             return true;
         }
 
-//      HttpSession session = request.getSession();
-//      int userID = 2;
-//      UserInfo userInfo = sysUserService.getUserInfoByUserID(userID);
-//      System.out.println(JsonUtil.toJson(userInfo));
-//      session.setAttribute(Constants.SessionKey.USER, userInfo);
-
         //如果登录了，会把用户信息存进session
         HttpSession session = request.getSession();
         Object account = session.getAttribute("account");
-        /*User userInfo = new User();
-        userInfo.setId(users.get(0).getId());
-        userInfo.setName(users.get(0).getName());
-        userInfo.setPassword(users.get(0).getPassword());*/
-        //开发环节的设置，不登录的情况下自动登录
-        /*if(userInfo==null && IGNORE_LOGIN){
-            userInfo = sysUserService.getUserInfoByUserID(2);
-            session.setAttribute(Constants.SessionKey.USER, userInfo);
-        }*/
         if (account == null) {
             /*log.info("尚未登录，跳转到登录界面");
             response.sendRedirect(request.getContextPath()+"signin");*/
 
             String requestType = request.getHeader("X-Requested-With");
-//          System.out.println(requestType);
             if (requestType != null && requestType.equals("XMLHttpRequest")) {
-                response.setHeader("sessionstatus", "timeout");
-//              response.setHeader("basePath",request.getContextPath());
+                response.setHeader("sessionStatus", "timeout");
+                response.setHeader("basePath", request.getContextPath());
                 response.getWriter().print("LoginTimeout");
                 return false;
             } else {
@@ -74,7 +52,6 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
             }
             return false;
         }
-//      log.info("用户已登录,userName:"+userInfo.getSysUser().getUserName());
         return true;
     }
 
@@ -85,13 +62,9 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
         path = path.substring(basePath.length());
         Set<String> notLoginPaths = new HashSet<>();
         //设置不进行登录拦截的路径：登录注册和验证码
-        //notLoginPaths.add("/");
-//        notLoginPaths.add("/index");
-        notLoginPaths.add("/user/login");
+//        notLoginPaths.add("/user/login");
         notLoginPaths.add("/user/register");
-        //notLoginPaths.add("/sys/logout");
-        //notLoginPaths.add("/loginTimeout");
 
-        return notLoginPaths.contains(path) || path.contains(".css") || path.contains("error") || path.contains("images") || path.contains(".js");
+        return notLoginPaths.contains(path);
     }
 }
--- web/src/main/java/cn/fzz/interceptor/WebSecurityConfig.java
+++ web/src/main/java/cn/fzz/interceptor/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package cn.fzz.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,8 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //addPathPattern后跟拦截地址，excludePathPatterns后跟排除拦截地址
-        registry.addInterceptor(new LoginInterceptor());
+        InterceptorRegistration registration = registry.addInterceptor(new LoginInterceptor());
+        registration.addPathPatterns("/**");                    //所有路径都被拦截
+        registration.excludePathPatterns("/","/user/login","/user/login","/user/login","/error/**","/static/**");       //添加不拦截路径
     }
 }
